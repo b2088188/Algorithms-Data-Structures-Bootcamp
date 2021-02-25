cec18103e8418c1580d55a9845351406bd2b2c5d
Shunze Lin <4A490101@stust.edu.tw>
2021-02-25 15:55:15 +0800

    FizzBuzz Exercise: Solution 1 - for loop to make condition and print different result based on the condition

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,27 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+	for (let i = 1; i <= n; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log('fizzbuzz');
+		} else if (i % 3 === 0) {
+			console.log('fizz');
+		} else if (i % 5 === 0) {
+			console.log('buzz');
+		} else console.log(i);
+	}
+}
+
+// function fizzBuzz(n) {
+// 	[...Array(n).keys()]
+// 		.map((el) => el + 1)
+// 		.forEach((el) => {
+// 			if (el % 3 === 0 && el % 5 === 0) return console.log('fizzbuzz');
+// 			if (el % 3 === 0) return console.log('fizz');
+// 			if (el % 5 === 0) return console.log('buzz');
+// 			console.log(el);
+// 		});
+// }
 
 module.exports = fizzBuzz;
