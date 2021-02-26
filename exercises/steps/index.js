668b7d2d68f24082b3955bc765521ef1e7cdd0f0
Shunze Lin <4A490101@stust.edu.tw>
2021-02-26 14:28:25 +0800

    Steps Exercise: Solution 2 - Recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,16 +17,31 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-	let i = 1;
-	while (i <= n) {
-		console.log(generateSymbol(i, '#') + generateSymbol(n - i, ' '));
-		i++;
+function steps(n, count = 0, str = '') {
+	if (count === n) return;
+	if (str.length === n) {
+		console.log(str);
+		return steps(n, count + 1);
 	}
+	if (str.length <= count) {
+		str += '#';
+	} else {
+		str += ' ';
+	}
+	return steps(n, count, str);
 }
 
-function generateSymbol(num, symbol) {
-	return [...Array(num).keys()].map((el) => symbol).join('');
-}
+// function steps(n, count = 1, str = '') {
+// 	if (count === n)
+// 		return console.log(
+// 			[...Array(count).keys()].map((el) => '#').join('') +
+// 				[...Array(n - count).keys()].map((el) => ' ').join('')
+// 		);
+// 	console.log(
+// 		[...Array(count).keys()].map((el) => '#').join('') +
+// 			[...Array(n - count).keys()].map((el) => ' ').join('')
+// 	);
+// 	return steps(n, count + 1);
+// }
 
 module.exports = steps;
