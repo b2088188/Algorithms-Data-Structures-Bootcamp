ae107be8ee53d0b81163d6638e08a2239b29e3c9
Shunze Lin <4A490101@stust.edu.tw>
2021-02-26 11:58:56 +0800

    Capitalize Exercise: Solution 1 - Split the string into array, and use reduce method to concat the capitalized character into the new string

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,12 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+	let arr = str.split(' ');
+	return arr.reduce((acc, cur, i) => {
+		if (i === 0) return cur[0].toUpperCase() + cur.slice(1);
+		return acc + ' ' + cur[0].toUpperCase() + cur.slice(1);
+	}, '');
+}
 
 module.exports = capitalize;
