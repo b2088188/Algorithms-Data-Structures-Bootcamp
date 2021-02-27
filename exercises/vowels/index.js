797f796ab7b71046b95cdad012ed38d251275e2f
Shunze Lin <4A490101@stust.edu.tw>
2021-02-27 09:03:43 +0800

    Find the vowel exercise: Solution 1 - Mapping a string functor.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,13 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+	let arr = [...str];
+	let vowelArr = ['a', 'e', 'i', 'o', 'u'];
+	return arr.reduce((acc, cur) => {
+		if (vowelArr.includes(cur) || vowelArr.includes(cur.toLowerCase())) return ++acc;
+		return acc;
+	}, 0);
+}
 
 module.exports = vowels;
